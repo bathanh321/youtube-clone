99576e52568c14a1a0c6a6690c89ee24f40e2b59
bathanh321 <thanhnbgch211319@fpt.edu.vn>
2025-02-09 23:39:40 +0700

    Authentication

--- src/modules/home/ui/components/home-sidebar/main-section.tsx
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -3,6 +3,7 @@
 import { SidebarGroup, SidebarGroupContent, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
 import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
 import Link from "next/link";
+import { useAuth, useClerk } from "@clerk/nextjs";
 
 const items = [
     {
@@ -24,6 +25,9 @@ const items = [
 ];
 
 export const MainSection = () => {
+    const clerk = useClerk();
+    const { isSignedIn } = useAuth();
+
     return (
         <SidebarGroup>
             <SidebarGroupContent>
@@ -34,7 +38,12 @@ export const MainSection = () => {
                                 tooltip={item.title}
                                 asChild
                                 isActive={false} // TODO: Change to look at current pathname
-                                onClick={() => { }} //TODO: Do something on click
+                                onClick={(e) => {
+                                    if(!isSignedIn && item.auth) {
+                                        e.preventDefault();
+                                        return clerk.openSignIn();
+                                    }
+                                 }}
                             >
                                 <Link href={item.url} className="flex items-center gap-4">
                                     <item.icon />
